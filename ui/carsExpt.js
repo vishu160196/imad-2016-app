6ddd9cd771edf72022467bf84294b0c04835ebf3
vishu160196 <vishu160196@gmail.com>
2016-11-23 17:00:19 +0530

    [imad-console] Updates ui/carsExpt.js

--- ui/carsExpt.js
+++ ui/carsExpt.js
@@ -1,44 +1,47 @@
 $(document).ready(function () {
     
+    $("#submitButton").hide();
+
     // Find car name for templating --- use a single js file for all car pages
     var carName;
     var url = $(location).attr('href');
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/a3' || url === 'http://localhost:8080/cars/a3/')
+    if (url === 'http://localhost:8080/cars/a3' || url === 'http://localhost:8080/cars/a3/')
         carName = "a3";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/a4' || url === 'http://localhost:8080/cars/a4/')
+    if (url === 'http://localhost:8080/cars/a4' || url === 'http://localhost:8080/cars/a4/')
         carName = "a4";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/s5' || url === 'http://localhost:8080/cars/s5/')
+    if (url === 'http://localhost:8080/cars/s5' || url === 'http://localhost:8080/cars/s5/')
         carName = "s5";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/a6' || url === 'http://localhost:8080/cars/a6/')
+    if (url === 'http://localhost:8080/cars/a6' || url === 'http://localhost:8080/cars/a6/')
         carName = "a6";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/a8l' || url === 'http://localhost:8080/cars/a8l/')
+    if (url === 'http://localhost:8080/cars/a8l' || url === 'http://localhost:8080/cars/a8l/')
         carName = "a8l";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/q3' || url === 'http://localhost:8080/cars/q3/')
+    if (url === 'http://localhost:8080/cars/q3' || url === 'http://localhost:8080/cars/q3/')
         carName = "q3";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/q5' || url === 'http://localhost:8080/cars/q5/')
+    if (url === 'http://localhost:8080/cars/q5' || url === 'http://localhost:8080/cars/q5/')
         carName = "q5";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/q7' || url === 'http://localhost:8080/cars/q7/')
+    if (url === 'http://localhost:8080/cars/q7' || url === 'http://localhost:8080/cars/q7/')
         carName = "q7";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/rs6' || url === 'http://localhost:8080/cars/rs6/')
+    if (url === 'http://localhost:8080/cars/rs6' || url === 'http://localhost:8080/cars/rs6/')
         carName = "rs6";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/rs7' || url === 'http://localhost:8080/cars/rs7/')
+    if (url === 'http://localhost:8080/cars/rs7' || url === 'http://localhost:8080/cars/rs7/')
         carName = "rs7";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/tt' || url === 'http://localhost:8080/cars/tt/')
+    if (url === 'http://localhost:8080/cars/tt' || url === 'http://localhost:8080/cars/tt/')
         carName = "tt";
 
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/r8' || url === 'http://localhost:8080/cars/r8/')
+    if (url === 'http://localhost:8080/cars/r8' || url === 'http://localhost:8080/cars/r8/')
         carName = "r8";
+
 
     $("#likeButton").click(function requestLikeEndPoint() {
         var request = new XMLHttpRequest;
@@ -81,57 +84,18 @@ $(document).ready(function () {
     });
 
     $("#feedback").click(function appearCommentButton() {
-        $("#appearFeedbackButton").html('<button class="buttonStyle" id="submitButton" onclick="submitFeedback()">Submit</button>');
+        $("#submitButton").show();
+    });
+
+    $("#submitButton").click(function () {
+
+        var feedback = document.getElementById("feedback").value;
+        var request = new XMLHttpRequest;
+        $("#prevFeedbacks").prepend("<img src=\"/person.png\" style=\"float:left\"/><div style=\"padding-left:50px\">" + feedback + "</div><br/><br/>");
+
+        request.open("GET", `/cars/${carName}/submit_feedback?feedback=` + feedback, true);
+        request.send(null);
+        $("#placeholderReplace").html('<h1 class ="fancy">Own this car? Leave a feedback!</h1><textarea cols="55" id="feedback" placeholder="Leave a feedback"></textarea>');
+        $("#submitButton").hide();
     });
 });
-
-function submitFeedback() {
-
-    var carName;
-    var url = $(location).attr('href');
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/a3' || url === 'http://localhost:8080/cars/a3/')
-        carName = "a3";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/a4' || url === 'http://localhost:8080/cars/a4/')
-        carName = "a4";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/s5' || url === 'http://localhost:8080/cars/s5/')
-        carName = "s5";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/a6' || url === 'http://localhost:8080/cars/a6/')
-        carName = "a6";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/a8l' || url === 'http://localhost:8080/cars/a8l/')
-        carName = "a8l";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/q3' || url === 'http://localhost:8080/cars/q3/')
-        carName = "q3";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/q5' || url === 'http://localhost:8080/cars/q5/')
-        carName = "q5";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/q7' || url === 'http://localhost:8080/cars/q7/')
-        carName = "q7";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/rs6' || url === 'http://localhost:8080/cars/rs6/')
-        carName = "rs6";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/rs7' || url === 'http://localhost:8080/cars/rs7/')
-        carName = "rs7";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/tt' || url === 'http://localhost:8080/cars/tt/')
-        carName = "tt";
-
-    if (url === 'http://vishu160196.imad.hasura-app.io:8080/cars/r8' || url === 'http://localhost:8080/cars/r8/')
-        carName = "r8";
-
-    var feedback = document.getElementById("feedback").value;
-    var request = new XMLHttpRequest;
-
-    $("#prevFeedbacks").prepend("<img src=\"/person.png\" style=\"float:left\"/><div style=\"padding-left:50px\">" + feedback + "</div><br/><br/>");
-
-    request.open("GET", `/cars/${carName}/submit_feedback?feedback=` + feedback, true);
-    request.send(null);
-    $("#placeholderReplace").html('<h1 class ="fancy">Own this car? Leave a feedback!</h1><textarea cols="55" id="feedback" placeholder="Leave a feedback"></textarea>');
-};
