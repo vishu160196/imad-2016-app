bacce53a512d135054b9605ed51f7f221e2626c9
vishu160196 <vishu160196@gmail.com>
2016-11-26 22:36:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(bodyParser.json());
 var config = {
     user: 'vishu160196', //env var: PGUSER
     database: 'vishu160196', //env var: PGDATABASE
-    password: process.env.DB_PASS, //env var: PGPASSWORD
-    host: 'localhost', // Server hosting the postgres database
+    password: process.env.DB_PASSWORD, //env var: PGPASSWORD
+    host: 'db.imad.hasura-app.io', // Server hosting the postgres database
     port: '5432', //env var: PGPORT
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
