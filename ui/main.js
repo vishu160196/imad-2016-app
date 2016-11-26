955548ede5b59eb5fb2c601f3ee1c837c0a31036
vishu160196 <vishu160196@gmail.com>
2016-11-26 18:49:37 +0530

    Add files via upload

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,103 @@
 // JavaScript source code
 
-$(document).ready(moveCar1);
-
-
-
-function moveCar1()
-{
+$(document).ready(function () {
     var car1 = $("#car1");
     var car2 = $("#car2");
     car1.animate({ left: '0%' }, 1000);
-    
+
     car2.animate({ left: '80%' }, 1000);
-    console.log(car2.css("right"));
-}
+    
+    $("#signup_button").click(function () {
+
+        var userName = $("#select_username");
+        var userPassword = $("#select_password");
+        var signupMessage = $("#signup_message");
+        
+        //check whether there is content in input fields
+        if(userName.val()==='' || userPassword.val()==='')
+        {
+            signupMessage.css("color", "red");
+            signupMessage.text("One of the entry fields is empty");
+        }
+
+        else {
+
+            //make the JSON request object
+            var request = {
+                userName: userName.val(),
+                userPassword: userPassword.val()
+            };
+
+            //make post request to /create-user endpoint
+            $.ajax({
+                url: "/create-user",
+                type: "POST",
+                data: JSON.stringify(request),
+                contentType: "application/json",
+                success: function (data) {
+                    //render the response received and also remove the field entries
+                    signupMessage.css("color", "green");
+                    signupMessage.text(data);
+                    userName.val('');
+                    userPassword: userPassword.val('');
+                },
+                error: function (data) {
+                    signupMessage.css("color", "red");
+                    signupMessage.text("Sorry this username is taken please try another one");
+                    userName.val('');
+                    userPassword: userPassword.val('');
+                }
+            });
+        }
+    });
+
+    $("#login_button").click(function () {
+        var userName = $("#username");
+        var userPassword = $("#password");
+        var loginMessage = $("#login_message");
+
+        //check whether there is content in input fields
+        if (userName.val() === '' || userPassword.val() === '') {
+            signupMessage.css("color", "red");
+            signupMessage.text("One of the entry fields is empty");
+        }
+
+        else {
+            var request = {
+                userName: userName.val(),
+                userPassword: userPassword.val()
+            };
+
+            $.ajax({
+                url: "/login",
+                type: "POST",
+                data: JSON.stringify(request),
+                contentType: "application/json",
+                success: function (data) {
+                    //render the response received and also remove the field entries                    
+                    userName.val('');
+                    userPassword: userPassword.val('');
+                    console.log(data);
+                },
+                error: function (data) {
+
+                    signupMessage.css("color", "red");
+
+                    if (data.status === 404) 
+                        signupMessage.text("There is no user by this name");
+
+                    else if (data.status === 401)
+                        signupMessage.text("Incorrect password");
+
+                    else if (data.status === 500)
+                        signupMessage.text("Something went wrong on the server Please try again in some time");                    
+                }
+            });
+        }
+    });
+});
+
+
+
+
 
